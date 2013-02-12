6ac8eb5190a71c27aae28385f8ea7c9b73fe54dc
Thabo Titus <thabotitus@live.com>
2013-02-12 20:51:17 +0200

    Gallery Image Captions

--- templates/views-view-fields--image-gallery--block.tpl.php
+++ templates/views-view-fields--image-gallery--block.tpl.php
@@ -1,13 +1,19 @@
 <article class="gallery_view_instance_container">
 
+
 	<div class="gallery_view_instance_img">
 		<?php print $fields['field_gal_img']->content; ?>
 	</div>
 
-	<header><?php print $fields['title']->content; ?></header>
+	<div class="overlay">
+		<div class="overlay_int">
 
-	<div class="gallery_view_instance_caption">
-		<?php print $fields['field_gal_img_caption']->content; ?>
+			<header><?php print $fields['title']->content; ?></header>
+
+			<div class="gallery_view_instance_caption">
+				<?php print $fields['field_gal_img_caption']->content; ?>
+			</div>
+		</div>
 	</div>
 
 
